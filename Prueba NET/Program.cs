38c0248353cdba11968f78a43f33ce4cfbdb0b6d
Victor <vd_ch2@hotmail.com>
2015-03-30 12:29:41 -0500

    Segundo cambio

---
+++ Prueba NET/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primera prueba
+            Console.WriteLine("Ingrese un numero");
+            int n = Console.ReadLine();
+            Console.WriteLine("Ingreso el numero: "+n);
+            //Fin de primera prueba
+        }
+    }
+}
